6b283dc4e8e08531061dd37a0912bcaeffc684d1
Josh Archer <jarcher@greenriver.edu>
2023-03-12 15:01:33 -0700

    Added code coverage configuration.

--- src/test/java/com/example/circleciw23/CircleCiW23ApplicationTests.java
+++ src/test/java/com/example/circleciw23/CircleCiW23ApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.circleciw23;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CircleCiW23ApplicationTests
@@ -16,12 +18,15 @@ class CircleCiW23ApplicationTests
     @Test
     public void testMe()
     {
-        //fail();
+        assertTrue(true);
+        int random = new Random().nextInt();
+        assertTrue(random > Integer.MIN_VALUE);
     }
 
     @Test
     public void anotherTest()
     {
-
+        String value = "hello";
+        assertNotNull(value);
     }
 }
