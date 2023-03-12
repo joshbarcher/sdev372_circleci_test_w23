787eb7f4a0cb9208c9c31c2ac030885ba3d548b4
Josh Archer <jarcher@greenriver.edu>
2023-03-12 15:21:49 -0700

    Added tests for a new dice class.

--- src/test/java/com/example/circleciw23/DiceTests.java
+++ src/test/java/com/example/circleciw23/DiceTests.java
@@ -1,9 +1,11 @@
 package com.example.circleciw23;
 
 import com.example.circleciw23.dice.Dice;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DiceTests
 {
@@ -15,15 +17,29 @@ public class DiceTests
         assertEquals(6, die.getSides());
     }
 
-    @Test
+    @RepeatedTest(10)
     public void rollTest()
     {
+        Dice dice = new Dice(10, "blue");
+        int roll = dice.roll();
+        rollCheck(roll, 1, 10);
+    }
 
+    private static void rollCheck(int roll, int min, int max)
+    {
+        assertTrue(roll <= max);
+        assertTrue(roll >= min);
     }
 
     @Test
     public void rollManyTest()
     {
+        Dice dice = new Dice(12, "orange");
+        int[] rollMany = dice.rollMany(10);
 
+        for (int i = 0; i < rollMany.length; i++)
+        {
+            rollCheck(rollMany[i], 1, 12);
+        }
     }
 }
