46e2dcaf516276d638b16cf30c5a43b946dffc6d
Josh Archer <jarcher@greenriver.edu>
2023-03-12 14:31:46 -0700

    Updated tests with a failure scenario.

--- src/test/java/com/example/circleciw23/CircleCiW23ApplicationTests.java
+++ src/test/java/com/example/circleciw23/CircleCiW23ApplicationTests.java
@@ -3,13 +3,19 @@ package com.example.circleciw23;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class CircleCiW23ApplicationTests
 {
-
     @Test
     void contextLoads()
     {
     }
 
+    @Test
+    public void testMe()
+    {
+        fail();
+    }
 }
